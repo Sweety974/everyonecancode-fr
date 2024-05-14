888f7d6a1c4999f2e7bba5dd35ab7b350e8b9627
Sweety974 <Zemmorgane@gmail.com>
2024-05-14 13:25:05 +0000

    Mise à jour du titre et de la couleur

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
-    <b-navbar fixed-top :mobile-burger="false">
+    <b-navbar fixed-top :mobile_burger="false">
       <template #brand>
         <div id="left"></div>
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar" class="title is-2">
-          Milligram
+        <b-navbar-item id="fontnavbar" class="title is-2" style="color:blueviolet">
+          sweetgram
         </b-navbar-item>
       </template>
     </b-navbar>
